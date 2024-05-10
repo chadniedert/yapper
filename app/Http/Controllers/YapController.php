7e6a77aad851c05bb163f731f7cdaade5cff7e74
Chad Niedert <chad.niedert@icloud.com>
2024-05-09 20:10:38 -0400

    Initial commit

---
+++ app/Http/Controllers/YapController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Yap;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class YapController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Yaps/Index', [
+            //
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->yaps()->create($validated);
+
+        return redirect(route('yaps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Yap $yap)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Yap $yap)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Yap $yap)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Yap $yap)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Yaps/Index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useForm, Head } from '@inertiajs/vue3';
+
+const form = useForm({
+    message: '',
+})
+
+</script>
+ 
+<template>
+    <Head title="Yaps" />
+ 
+    <AuthenticatedLayout>
+        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <form @submit.prevent="form.post(route('yaps.store'), { onSuccess: () => form.reset() })">
+                <textarea
+                    v-model="form.message"
+                    placeholder="What noise do you need to create?"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                ></textarea>
+                <InputError :message="form.errors.message" class="mt-2" />
+                <PrimaryButton class="mt-4">Yap</PrimaryButton>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+</template>
